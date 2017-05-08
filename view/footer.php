f434cb1bd968335f82c57b507910dfc5059785f2
Deepak Rameshkumar <dr439@afsconnect2.njit.edu>
2017-05-08 10:30:40 -0400

    create footer

---
+++ view/footer.php
@@ -0,0 +1,19 @@
+<html>
+<style>
+.welcome{
+position:bottom;
+background:#a9a9a9;
+}
+p {
+margin-right: 5px;
+font-family: cursive;
+color:#fff;
+text-align: center;
+}
+</style>
+<body>
+<div class="Welcome to the Page">
+<strong> <p>  To Do List 2017</p></strong>
+<strong> <p> Developed By - Balakumar Shanmugam</p></strong>
+</div>
+</body>
